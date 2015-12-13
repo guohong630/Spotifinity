1a2dcf5d13608f4d5fe15edbb5333a6849176e31
Weixin Wu <weixinwu@dyn-209-2-234-128.dyn.columbia.edu>
2015-12-12 20:56:33 -0500

    modify home page

--- Javascript/showList.js
+++ Javascript/showList.js
@@ -1,3 +1,8 @@
+var artist = {
+	"123" : "celia"
+}
+localStorage.setItem("staredArtist", JSON.stringify(artist));
+
 function showProfile() {
 	var retrievedObject = localStorage.getItem("profiles");
 	var catalogs = JSON.parse(retrievedObject);
@@ -9,8 +14,8 @@ function showProfile() {
 		li.setAttribute("id", "list_profile_" + key);
 		var profile = catalogs[key];
 		a = document.createElement('a');
-		a.setAttribute('id', key + "unique_number");
-		a.href = "javascript:showTasteProfile(" + key + ", " + catalogs[key] +")";
+		a.setAttribute('id', key + "_unique_number");
+		a.href = "javascript:showTasteProfile(" + key + ", " + profile +")";
 		a.innerHTML = profile;
 		li.appendChild(a);
 		side_bar.appendChild(li);
@@ -25,5 +30,20 @@ function updateDistance() {
 }
 
 function showArtist(){
-	
+	var retrievedObject = localStorage.getItem("staredArtist");
+	var staredArtist = JSON.parse(retrievedObject); 
+	var side_bar = document.getElementById("side_bar2");
+	console.log(side_bar)
+	for (var key in staredArtist){
+		console.log(key)
+		var li = document.createElement("li");
+		li.setAttribute("id", "list_artist_" + key);
+		var artist = staredArtist[key];
+		a = document.createElement('a');
+		a.setAttribute('id', key + "_unique_number");
+		a.href = "javascript:showStaredArtisit(" + key + ", " + artist +")";
+		a.innerHTML = artist;
+		li.appendChild(a);
+		side_bar.appendChild(li);
+	}
 }
